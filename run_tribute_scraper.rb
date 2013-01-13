ca0e816f1d2bc7cd2d4bca97036d5b9bd21a6d7c
root <birk5437@gmail.com>
2013-01-13 21:59:27 +0000

    final tweaks

--- run_tribute_scraper.rb
+++ run_tribute_scraper.rb
@@ -1,9 +1,10 @@
-require '/Users/burke/workbench/tribute_scraper/tribute_scraper.rb'
+require 'tribute_scraper.rb'
+require 'date'
 
 ts = TributeScraper.new
 ts.do_all
 
-links_list = ts.get_local_links_list
+links_list = ts.get_local_links_list.reverse
 
 
 File.open("index.html", "w") do |f|
@@ -14,7 +15,8 @@ File.open("index.html", "w") do |f|
   f.puts "<script src='bootstrap/js/bootstrap.min.js'></script>"
   f.puts "<h3>PDFTributeScraper</h3>"
   f.puts "<p>This scraper gets all of the PDF filetype links from within the links posted on <a href='https://twitter.com/search/realtime?q=%23PDFTribute&src=hash'>#PDFTribute</a></p>"
-  f.puts "[<a href='current_list.txt'>full list of PDF files</a>]<br />"
+  f.puts "<p>Last Updated: #{DateTime.now.month}/#{DateTime.now.day}/#{DateTime.now.year} #{DateTime.now.hour}:#{DateTime.now.min}:#{DateTime.now.sec} UTC<br />"
+  f.puts "[<a href='current_links.txt'>full list of PDF files</a>]<br />"
   f.puts "[<a href='https://github.com/birk5437/tribute_scraper'>source code</a>]<br />"
   f.puts "<hr /><br />"
   links_list.each do |ltw|
